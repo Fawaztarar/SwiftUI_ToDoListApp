11c87ccb016788a31dcaf41a98f2f9a3ee203954
Fawaztarar <fawaztarr@gmail..com>
2024-04-15 14:45:28 +0100

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Fawaz Tarar on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
