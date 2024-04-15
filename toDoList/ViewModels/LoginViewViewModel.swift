2675576b066617cca6413187e7544c2a811a56fe
Fawaztarar <fawaztarr@gmail..com>
2024-04-15 23:18:01 +0100

    FirebaseAuth loginview

--- toDoList/ViewModels/LoginViewViewModel.swift
+++ toDoList/ViewModels/LoginViewViewModel.swift
@@ -4,18 +4,34 @@
 //
 //  Created by Fawaz Tarar on 15/04/2024.
 //
-
+import FirebaseAuth
 import Foundation
 
 class LoginViewViewModel: ObservableObject {
     @Published var email = ""
     @Published var password = ""
+    @Published var errorMessage = ""
     
     func login() {
-        // Perform login action
+        guard validate() else {
+            return
+        }
+        // try to login
+        Auth.auth().signIn(withEmail: email, password: password)
+
+  
     }
-    func validate() -> Bool {
-        return !email.isEmpty && !password.isEmpty
-            
+    private func validate() -> Bool {
+        errorMessage = ""
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty && !password.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessage = "Please fill in all fields"
+            return false
+        }
+        // email with @ and .
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Please enter a valid email"
+            return false
+        }
+        return true    
     }
 }
--- toDoList/Views/LoginView.swift
+++ toDoList/Views/LoginView.swift
@@ -4,17 +4,15 @@
 //
 //  Created by Fawaz Tarar on 15/04/2024.
 //
+
 import SwiftUI
+
 struct LoginView: View {
-    @StateObject var viewModel = LoginViewViewModel()
+    @ObservedObject var viewModel = LoginViewViewModel()
     
     var body: some View {
-        
         NavigationView {
             VStack {
-                
-                
-                // Heading Text "To Do List" subheading "Get your tasks done"
                 Text("To Do Manager ")
                     .font(.system(size: 40))
                     .fontWeight(.bold)
@@ -24,10 +22,13 @@ struct LoginView: View {
                     .font(.system(size: 25))
                     .foregroundColor(.gray)
                     .padding(.bottom, 120)
-                       
                 
-
-                // text fields for email and password with login button    
+                if !viewModel.errorMessage.isEmpty {
+                    Text(viewModel.errorMessage)
+                        .foregroundColor(.red)
+                        .padding(.bottom, 20)
+                }
+                       
                 TextField("Email", text: $viewModel.email)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .keyboardType(.emailAddress)
@@ -42,7 +43,7 @@ struct LoginView: View {
                     .padding(.bottom, 20)
                 
                 Button(action: {
-                    // Perform login action
+                    viewModel.login()
                 }) {
                     Text("Login")
                         .foregroundColor(.white)
@@ -53,7 +54,6 @@ struct LoginView: View {
                 }
                 .padding(.horizontal)
 
-                // Bottom Text "Don't have an account? Sign Up" with button to navigate to SignUpView
                 Text("Don't have an account?")
                     .foregroundColor(.gray)
                     .padding(.top, 20)
@@ -63,9 +63,7 @@ struct LoginView: View {
                     .padding(.bottom, 20)
                 
             }
-          
         }
-    
     }
 }
 
