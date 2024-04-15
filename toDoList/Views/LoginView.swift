1abe3bf9255d6b72398d839fd9260f5752dffd48
Fawaztarar <fawaztarr@gmail..com>
2024-04-15 17:21:48 +0100

    mainview and loginview

--- toDoList/Views/LoginView.swift
+++ toDoList/Views/LoginView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Fawaz Tarar on 15/04/2024.
 //
-
 import SwiftUI
 
 struct LoginView: View {
@@ -13,59 +12,63 @@ struct LoginView: View {
     
     var body: some View {
         
-        VStack {
-            // Heading Text "To Do List" subheading "Get your tasks done"
-            Text("To Do List ")
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.bottom, 25)
+        NavigationView {
+            VStack {
+                // Heading Text "To Do List" subheading "Get your tasks done"
+                Text("To Do Manager ")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 25)
 
-            Text("Get your tasks done")
-                .font(.subheadline)
-                .foregroundColor(.gray)
-                .padding(.bottom, 20)
+                Text("Get your tasks done")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 20)
             
 
-            // text fields for email and password with login button    
-            TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+                // text fields for email and password with login button    
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                    .padding(.bottom, 10)
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                    .padding(.bottom, 20)
+                
+                Button(action: {
+                    // Perform login action
+                }) {
+                    Text("Login")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
                 .padding(.horizontal)
-                .padding(.bottom, 10)
-                
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-                .padding(.bottom, 20)
-                
-            Button(action: {
-                // Perform login action
-            }) {
-                Text("Login")
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-            .padding(.horizontal)
 
-            // Bottom Text "Don't have an account? Sign Up" with button to navigate to SignUpView
-        }
-
-            Text("Don't have an account?")
-                .foregroundColor(.gray)
-                .padding(.top, 20)
+                // Bottom Text "Don't have an account? Sign Up" with button to navigate to SignUpView
+                Text("Don't have an account?")
+                    .foregroundColor(.gray)
+                    .padding(.top, 20)
                 
-            NavigationLink(destination: RegisterView()) {
-                Text("Sign Up")
+                NavigationLink("Create an Account", destination: RegisterView()) 
                     .foregroundColor(.blue)
-                    .underline()
+                    .padding(.bottom, 20)
+                
             }
           
         }
+    
     }
-
-
-#Preview {
-    LoginView()
 }
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
+
+
--- toDoList/Views/MainView.swift
+++ toDoList/Views/MainView.swift
@@ -9,12 +9,11 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        NavigationView {
-            LoginView()
-        }
-        
+        LoginView()
     }
-}
+        
+ }
+
 
 #Preview {
     MainView()
