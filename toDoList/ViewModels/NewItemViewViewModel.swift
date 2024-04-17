8ab06dbf7aa7fb134aa66c7b888a5dddea792f1d
Fawaztarar <fawaztarr@gmail..com>
2024-04-17 15:19:22 +0100

    newItemView with validation

--- toDoList/ViewModels/NewItemViewViewModel.swift
+++ toDoList/ViewModels/NewItemViewViewModel.swift
@@ -10,14 +10,26 @@ import Foundation
 class NewItemViewViewModel: ObservableObject {
     
     @Published var title: String = ""
-    @Published var description: String = ""
     @Published var dueDate: Date = Date()
+    @Published var showAlert: Bool = false
     
     init() {
     }
 
     func save() {
-        
+
+    }
+    var canSave: Bool { 
+    guard !title.trimmingCharacters(in:.whitespaces).isEmpty else {
+        return false
+    }
+
+    guard dueDate > Date().addingTimeInterval(-86400) else {
+    
+        return false
+    }
+
+    return true
     }
 }
 
--- toDoList/Views/NewItemView.swift
+++ toDoList/Views/NewItemView.swift
@@ -6,6 +6,7 @@
 //  Created by Fawaz Tarar on 15/04/2024.
 //
 
+
 import SwiftUI
 
 struct NewItemView: View {
@@ -30,15 +31,22 @@ struct NewItemView: View {
                 Spacer() // Add space above the button
                 
                 Button(action: {
-                    viewModel.save() 
-                    newitemPresented = false
+                    if viewModel.canSave {
+                        viewModel.save()
+                        newitemPresented = false
+                    } else {
+                        viewModel.showAlert = true
+                    }
                 }) {
                     Text("Save")
                         .foregroundColor(.white)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(Color.pink)
-                                .cornerRadius(10)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.pink)
+                        .cornerRadius(10)
+                }
+                .alert(isPresented: $viewModel.showAlert) {
+                    Alert(title: Text("Error"), message: Text("Please enter a title and a due date not be in past"), dismissButton: .default(Text("OK")))
                 }
             }
         }
@@ -51,6 +59,3 @@ struct NewItemView_Previews: PreviewProvider {
             return true }, set: { _ in }))
     }
 }
-    
-
-
